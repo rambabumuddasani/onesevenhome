9f9385e2037a9b8af2ba67ca342bf240a6bfba95
SurenderCh <surendervarmac@gmail.com>
2017-07-30 22:43:03 +0530

    merged contactus code

---
+++ one7homeWar/src/main/java/com/salesmanager/shop/admin/controller/merchant/MerchantStoreResponse.java
@@ -0,0 +1,57 @@
+package com.salesmanager.shop.admin.controller.merchant;
+
+public class MerchantStoreResponse {
+	
+	String phone;
+	String eMail;
+	String address;
+	String city;
+	String country;
+	String state;
+	String postalCode;
+	
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String geteMail() {
+		return eMail;
+	}
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	
+}
