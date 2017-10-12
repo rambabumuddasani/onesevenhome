25adb957957f11a15b004e9cd03d49a4c7453b2a
sm19811130 <sm19811130@gmail.com>
2017-10-12 21:19:06 +0530

    siva-filter creation and product updation

---
+++ core/src/main/java/com/salesmanager/core/business/services/catalog/product/filter/FilterTypeService.java
@@ -0,0 +1,10 @@
+package com.salesmanager.core.business.services.catalog.product.filter;
+
+import com.salesmanager.core.business.services.common.generic.SalesManagerEntityService;
+import com.salesmanager.core.model.catalog.product.filter.FilterType;
+
+
+public interface FilterTypeService extends SalesManagerEntityService<Long, FilterType>{
+
+
+}
--- one7homeWar/src/main/java/com/salesmanager/shop/admin/controller/categories/FilterController.java
+++ one7homeWar/src/main/java/com/salesmanager/shop/admin/controller/categories/FilterController.java
@@ -3,9 +3,11 @@ package com.salesmanager.shop.admin.controller.categories;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
@@ -33,13 +35,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.salesmanager.core.business.services.catalog.category.CategoryService;
+import com.salesmanager.core.business.services.catalog.product.ProductService;
 import com.salesmanager.core.business.services.catalog.product.filter.FilterService;
+import com.salesmanager.core.business.services.catalog.product.filter.FilterTypeService;
 import com.salesmanager.core.business.services.reference.country.CountryService;
 import com.salesmanager.core.business.services.reference.language.LanguageService;
 import com.salesmanager.core.business.utils.ajax.AjaxResponse;
 import com.salesmanager.core.model.catalog.category.Category;
 import com.salesmanager.core.model.catalog.category.CategoryDescription;
+import com.salesmanager.core.model.catalog.product.Product;
 import com.salesmanager.core.model.catalog.product.filter.Filter;
+import com.salesmanager.core.model.catalog.product.filter.FilterType;
 import com.salesmanager.core.model.merchant.MerchantStore;
 import com.salesmanager.core.model.reference.language.Language;
 import com.salesmanager.shop.admin.model.web.Menu;
@@ -62,8 +68,13 @@ public class FilterController {
 	FilterService filterService;
 	
 	@Inject
+	FilterTypeService filterTypeService;
+
+	@Inject
 	LabelUtils messages;
 
+	@Inject
+	private ProductService productService;
 
 	@RequestMapping(value="/getFiltersByCategory/{categoryId}", method = RequestMethod.GET, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
@@ -98,5 +109,129 @@ public class FilterController {
 		return filterResponse;
 	}
 
-
+    @RequestMapping(value="/createFilter", method = RequestMethod.POST, 
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public CreateFilterResponse createProduct(@RequestBody CreateFilterTypeRequest filterTypeRequest) throws Exception {
+    	CreateFilterResponse createFilterResponse = new CreateFilterResponse();
+		String categoryName = filterTypeRequest.getCategoryName();
+		categoryName = categoryName.replaceAll("_", " ");
+		Category category = categoryService.getByCategoryCode(categoryName);
+		
+		if(category == null){
+			System.out.println("Category does not exists with name:"+categoryName);
+			createFilterResponse.setStatus(false);
+			createFilterResponse.setErrorMessage("Category does not exists with name:"+categoryName);
+			return createFilterResponse;
+		} else {
+			if(!("").equals(filterTypeRequest.getFilterName())){
+				Filter filter = new Filter();
+				filter.setCategory(category);
+				filter.setFilterName(filterTypeRequest.getFilterName());
+				filterService.save(filter);
+				createFilterResponse.setCategoryId(category.getId());
+				createFilterResponse.setFilterName(filterTypeRequest.getFilterName());
+				createFilterResponse.setStatus(true);
+			}
+		}    	
+    	return createFilterResponse;
+    }
+    
+    @RequestMapping(value="/createFilterTypes", method = RequestMethod.POST, 
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public CreateFilterResponse createFilterTypes(@RequestBody CreateFilterTypeRequest filterTypeRequest) throws Exception {
+    	CreateFilterResponse createFilterResponse = new CreateFilterResponse();
+		String categoryName = filterTypeRequest.getCategoryName();
+		categoryName = categoryName.replaceAll("_", " ");
+		Category category = categoryService.getByCategoryCode(categoryName);
+		
+		if(category == null){
+			System.out.println("Category does not exists with name:"+categoryName);
+			createFilterResponse.setStatus(false);
+			createFilterResponse.setErrorMessage("Category does not exists with name:"+categoryName);
+			return createFilterResponse;
+		} else {
+			Filter filter = getFilterID(filterTypeRequest.getFilterName(), categoryName);
+			if(filter == null){
+				createFilterResponse.setStatus(false);
+				createFilterResponse.setErrorMessage("Filter does not exists with name:"+filterTypeRequest.getFilterName());
+				return createFilterResponse;
+			}
+			if(filterTypeRequest.getFilterTypes() != null){
+				FilterType filterType = null;
+				filterType.setFilter(filter);
+				List<FilterType> filterTypesList = new ArrayList<FilterType>();
+				for(String filterTypeName:filterTypeRequest.getFilterTypes()) {
+					filterType = new FilterType();
+					filterType.setFilterTypeName(filterTypeName);
+					filterTypesList.add(filterType);
+				}
+				filter.setFilterTypes(filterTypesList);
+				filterService.update(filter);
+				createFilterResponse.setCategoryId(category.getId());
+				createFilterResponse.setFilterName(filterTypeRequest.getFilterName());
+				createFilterResponse.setStatus(true);
+			}
+		}    	
+    	return createFilterResponse;
+    }
+    
+    @RequestMapping(value="/addProductToFilter", method = RequestMethod.POST, 
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public CreateFilterResponse addProductToFilter(@RequestBody CreateFilterTypeRequest filterTypeRequest) throws Exception {
+    	CreateFilterResponse createFilterResponse = new CreateFilterResponse();
+		String categoryName = filterTypeRequest.getCategoryName();
+		categoryName = categoryName.replaceAll("_", " ");
+		Category category = categoryService.getByCategoryCode(categoryName);
+		
+		if(filterTypeRequest.getProductId() == null){
+			createFilterResponse.setStatus(false);
+			createFilterResponse.setErrorMessage("Product ID can not be empty");
+			return createFilterResponse;
+		} else {
+			Product dbProduct = productService.getById(filterTypeRequest.getProductId());
+			Filter filter = null;
+			if(dbProduct == null) {
+				createFilterResponse.setStatus(false);
+				createFilterResponse.setErrorMessage("Product does not exists with product id:"+filterTypeRequest.getProductId());
+				return createFilterResponse;
+			}
+			
+			if(filterTypeRequest.getFilterTypeIds() != null){
+				FilterType filterType = null;
+				filterType.setFilter(filter);
+				Set<FilterType> filters = new HashSet<FilterType>();
+				for(Long filterId:filterTypeRequest.getFilterTypeIds()) {
+					filterType = filterTypeService.getById(filterId);
+					filters.add(filterType);
+				}
+				dbProduct.setFilters(filters);
+				productService.update(dbProduct);
+				createFilterResponse.setCategoryId(category.getId());
+				createFilterResponse.setFilterName(filterTypeRequest.getFilterName());
+				createFilterResponse.setStatus(true);
+			}
+		}    	
+    	return createFilterResponse;
+    }
+    
+	private Filter getFilterID(String filterName,String categoryId) throws Exception {
+		System.out.println("inside getFilterID");
+		Filter filter = null;
+		List<Filter> filters = filterService.listByCategoryCode(categoryId);
+		for(Filter f:filters){
+			if(filterName.equals(f.getFilterName())) {
+				filter = f;
+				break;
+			}
+		}
+		
+		return filter;
+	}
+   	
 }
