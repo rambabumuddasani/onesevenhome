ead9e25b004d4e26ef42c031323eae82af94271f
ram <rambabu0006@gmail.com>
2017-11-25 08:38:22 +0530

    Order Repo pagination

--- core/src/main/java/com/salesmanager/core/business/repositories/order/OrderRepository.java
+++ core/src/main/java/com/salesmanager/core/business/repositories/order/OrderRepository.java
@@ -2,8 +2,11 @@ package com.salesmanager.core.business.repositories.order;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.salesmanager.core.model.order.Order;
 
@@ -14,4 +17,8 @@ public interface OrderRepository extends JpaRepository<Order, Long>, OrderReposi
     
     @Query("select distinct o from Order o join fetch o.orderProducts op join fetch o.orderTotal ot left join fetch o.orderHistory oh left join fetch op.downloads opd left join fetch op.orderAttributes opa left join fetch op.prices opp where o.customerId = ?1")
 	List<Order> findOrdersByCustomer(Long id);
+    
+    @Query(value = "select distinct o from Order o join fetch o.orderProducts op join fetch o.orderTotal ot left join fetch o.orderHistory oh left join fetch op.downloads opd left join fetch op.orderAttributes opa left join fetch op.prices opp where o.customerId = ?1",
+    	       countQuery = "select count(distinct o) from Order o join fetch o.orderProducts op join fetch o.orderTotal ot left join fetch o.orderHistory oh left join fetch op.downloads opd left join fetch op.orderAttributes opa left join fetch op.prices opp where o.customerId = ?1")
+    Page<Order> findPaginatedOrdersByCustomer(@Param("id") Long id,Pageable pageable);
 }
